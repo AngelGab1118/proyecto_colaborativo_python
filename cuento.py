aceebe2bbf54bed91d6e62cbd90b1398a42d53d6
[Roryd Espinosa] <[Roryd.Espinosa@uip.edu.pa]>
2026-03-09 15:16:23 -0500

    Se agrega final al cuento

--- cuento.py
+++ cuento.py
@@ -3,3 +3,4 @@ def imprimir_cuento():
     print("Un día decidió trabajar con GitHub para colaborar con su equipo.")
     print("Después de mucho esfuerzo lograron terminar su proyecto.")
     print("Y aprendieron que trabajar en equipo hace el trabajo más fácil.")
+    print("Fin del cuento.")
